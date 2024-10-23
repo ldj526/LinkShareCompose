6d8eb00cb7abeffe789032011bb10485dd7ecbcf
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-10-23 15:17:32 +0900

    feat: 첫 화면을 위한 Activity, FirebaseAuth 추가

---
+++ app/src/main/java/com/example/linksharecompose/StartActivity.kt
@@ -0,0 +1,34 @@
+package com.example.linksharecompose
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import com.google.firebase.auth.FirebaseAuth
+
+class StartActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LaunchedEffect(Unit) {
+                navigateToNextScreen()
+            }
+        }
+    }
+
+    private fun navigateToNextScreen() {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        val nextActivity = if (currentUser != null) {
+            MainActivity::class.java
+        } else {
+            AuthActivity::class.java
+        }
+
+        startActivity(Intent(this, nextActivity))
+        finish()
+    }
+}
