0a0bebcaa2a2d465f6b53de3575c9d4990875084
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-10-28 20:43:22 +0900

    feat: 로그인 및 닉네임 상태 확인 후 화면 이동 로직 추가
    - 로그인 상태뿐만 아니라 닉네임 설정 여부도 확인하여 MainActivity 또는 AuthActivity로 이동하도록 변경

--- app/src/main/java/com/example/linksharecompose/StartActivity.kt
+++ app/src/main/java/com/example/linksharecompose/StartActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.LaunchedEffect
 import com.example.linksharecompose.auth.AuthActivity
+import com.example.linksharecompose.auth.FirebaseCollection
 import com.google.firebase.auth.FirebaseAuth
 
 class StartActivity : ComponentActivity() {
@@ -23,13 +24,33 @@ class StartActivity : ComponentActivity() {
     private fun navigateToNextScreen() {
         val currentUser = FirebaseAuth.getInstance().currentUser
 
-        val nextActivity = if (currentUser != null) {
-            MainActivity::class.java
-        } else {
-            AuthActivity::class.java
-        }
+        if (currentUser != null) {
+            val userId = currentUser.uid
+            val userRef = FirebaseCollection.userCollection.document(userId)
 
-        startActivity(Intent(this, nextActivity))
-        finish()
+            userRef.get().addOnSuccessListener { document ->
+                if (document != null && document.exists()) {
+                    val nickname = document.getString("nickname")
+                    if (nickname.isNullOrEmpty()) {
+                        // 닉네임 존재할 때
+                        startActivity(Intent(this, AuthActivity::class.java))
+                    } else {
+                        // 닉네임 존재하지 않을 때
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }
+                } else {
+                    // Firestore에 data가 없을 때
+                    startActivity(Intent(this, AuthActivity::class.java))
+                }
+                finish()
+            }.addOnFailureListener {
+                startActivity(Intent(this, AuthActivity::class.java))
+                finish()
+            }
+        } else {
+            // 로그인이 되어있지 않을 때
+            startActivity(Intent(this, AuthActivity::class.java))
+            finish()
+        }
     }
 }
